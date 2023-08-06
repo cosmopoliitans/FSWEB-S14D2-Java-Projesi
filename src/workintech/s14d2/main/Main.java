38cc59bc2d148deacf854f1b8859b32b1ab4e579
cosmopoliitans <elfkyn52@gmail.com>
2023-08-06 21:07:44 +0300

    Java Composition

--- src/workintech/s14d2/main/Main.java
+++ src/workintech/s14d2/main/Main.java
@@ -6,16 +6,16 @@ import workintech.s14d2.model.*;
 
 public class Main {
     public static void main(String[] args) {
-        Bedroom bedroom = new Bedroom("Ahmet",
-                        new Wall(Direction.NORTH),
-                        new Wall(Direction.SOUTH),
-                        new Wall(Direction.WEST),
-                        new Wall(Direction.EAST),
-                        new Ceiling(3, PaintColor.RED),
-                new Bed("Çift Kişilik", 4, 1, 2, 2),
-                new Lamp(LampType.LAVA, true, 4),
-                new Wardrobe(2, 4, 40),
-                new Carpet(3,5, PaintColor.RED));
+        Bedroom bedroom = new Bedroom("Elif",
+                                new Wall(Direction.NORTH),
+                                new Wall(Direction.SOUTH),
+                                new Wall(Direction.WEST),
+                                new Wall(Direction.EAST),
+                                new Ceiling(3, PaintColor.RED),
+                                new Lamp(LampType.LAVA, true, 4),
+                                new Bed("Çift Kişilik", 5, 2, 2, 2),
+                                new Wardrobe(2, 4, 40),
+                                new Carpet(3,5, PaintColor.RED));
 
         bedroom.getBed().make();
         bedroom.getLamp().turnOn();
